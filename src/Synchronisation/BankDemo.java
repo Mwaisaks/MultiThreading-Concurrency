44a38319515d88a273f9fdf2e4023fc4774d035b
Phenny <mwaisakaphenny@gmail.com>
2025-03-05 05:02:09 +0300

    Thread synchronisation :\

--- src/Synchronisation/BankDemo.java
+++ src/Synchronisation/BankDemo.java
@@ -4,8 +4,41 @@ public class BankDemo {
     public static void main(String[] args) {
         BankAccount account = new BankAccount(1000);
 
-        Runnable depossitTask = () -> {
+        Runnable depositTask = () -> {
+            for (int i = 0; i < 5;i++){
+                account.deposit(100);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread interrupted!");
+                }
+            }
+        };
 
+        Runnable withDrawTask = () -> {
+            for (int i = 0;i <5;i++){
+                account.withdraw(150);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread interrupted!");
+                }
+            }
+        };
+
+        Thread depositThread = new Thread(depositTask);
+        Thread withdrawThread = new Thread(withDrawTask);
+
+        depositThread.start();
+        withdrawThread.start();
+
+        try {
+            depositThread.join();
+            withdrawThread.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted!");
         }
+
+        System.out.println("Final balance: " + account.getBalance());
     }
 }
